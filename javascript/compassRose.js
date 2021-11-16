6001b8b478b87001c7ea728d5a5ed40b1e74137c
Anders Olofsson <andon50@gmail.com>
2021-11-16 16:26:37 +0100

    Arrows
    Arrows

--- javascript/compassRose.js
+++ javascript/compassRose.js
@@ -2,50 +2,50 @@
 function compassRose(n) {
   var grad = n;
   if (grad <= 11.25 || grad > 337.5 + 11.25) {
-    return "Nord";
+    return "N";
   }
   if (grad > 315 + 11.25) {
-    return "NordNordVäst";
+    return "NNV";
   }
   if (grad > 292.5 + 11.25) {
-    return "NordVäst";
+    return "NV";
   }
   if (grad > 270 + 11.25) {
-    return "VästNordVäst";
+    return "VNV";
   }
   if (grad > 247.5 + 11.25) {
-    return "Väst";
+    return "V";
   }
   if (grad > 225 + 11.25) {
-    return "VästSydVäst";
+    return "VSV";
   }
   if (grad > 202.5 + 11.25) {
-    return "SydVäst";
+    return "SV";
   }
   if (grad > 180 + 11.25) {
-    return "SydSydVäst";
+    return "SSV";
   }
   if (grad > 157.5 + 11.25) {
-    return "Syd";
+    return "S";
   }
   if (grad > 135 + 11.25) {
-    return "SydSydOst";
+    return "SSO";
   }
   if (grad > 112.5 + 11.25) {
-    return "SydOst";
+    return "SO";
   }
   if (grad > 90 + 11.25) {
-    return "OstSydOst";
+    return "OSO";
   }
   if (grad > 67.5 + 11.25) {
-    return "Ost";
+    return "O";
   }
   if (grad > 45 + 11.25) {
-    return "OstNordOst";
+    return "ONO";
   }
   if (grad > 22.5 + 11.25) {
-    return "NordOst";
+    return "NO";
   } else {
-    return "NordNordOst";
+    return "NNO";
   }
 }
